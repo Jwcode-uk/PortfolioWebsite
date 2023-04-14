5f19a9af1f3f89d3b86776fd6ba1dc5508e9280c
Jonno <jonathanwhite0000@gmail.com>
2023-04-14 20:22:34 +0100

    lol

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
     <div className="App App-header geometric-background">
       <App />
     </div>
